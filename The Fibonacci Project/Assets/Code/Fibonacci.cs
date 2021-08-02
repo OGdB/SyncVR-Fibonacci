c51bd5c96693f7af2bf06c1870afe6a72fd9dc43
OGdB <58260499+OGdB@users.noreply.github.com>
2021-08-02 19:34:03 +0200

    Basics: Fibonacci Script & SkyBox
    Fibonacci Script which gets triggered and shows the next sequence number if clicking on button.
    Slowly rotating space skybox left from another project just because it's cool and quick.

---
+++ The Fibonacci Project/Assets/Code/Fibonacci.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(AudioSource))]
+public class Fibonacci : MonoBehaviour
+{
+    [SerializeField] private int N1 = 0;
+    [SerializeField] private int N2 = 1;
+
+    [Header("Assignables")]
+    [Space(5)]
+    [SerializeField] private TMPro.TextMeshPro n1Text = null;
+    [SerializeField] private TMPro.TextMeshPro n2Text = null;
+    [SerializeField] private TMPro.TextMeshPro n3Text = null;
+
+    private AudioSource audioSource = null;
+    [SerializeField] private AudioClip clip = null;
+
+    private void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+
+        n1Text.text = N1.ToString();
+        n2Text.text = N2.ToString();
+    }
+
+    public void GetNextFibonacciSequence()
+    {
+        int newNumber = N1 + N2;
+
+        N1 = N2;
+        N2 = newNumber;
+
+        n1Text.text = N1.ToString();
+        n2Text.text = N2.ToString();
+
+        n3Text.text = newNumber.ToString();
+
+        audioSource.Play();
+    }
+}
---
+++ The Fibonacci Project/Assets/RotateSkybox.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+public class RotateSkybox : MonoBehaviour
+{
+    [SerializeField] private float speed = 1;
+    private void FixedUpdate()
+    {
+        RenderSettings.skybox.SetFloat("_Rotation", Time.time * speed);
+    }
+}
