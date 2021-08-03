5d1d555c0fdc975116a3ddc984869968c2c116b4
OGdB <58260499+OGdB@users.noreply.github.com>
2021-08-03 12:40:48 +0200

    First Basic iteration with simple animation

--- The Fibonacci Project/Assets/Code/Fibonacci.cs
+++ The Fibonacci Project/Assets/Code/Fibonacci.cs
@@ -1,26 +1,43 @@
-﻿using UnityEngine;
+﻿using System.Collections;
+using UnityEngine;
 
-[RequireComponent(typeof(AudioSource))]
 public class Fibonacci : MonoBehaviour
 {
     [SerializeField] private int N1 = 0;
     [SerializeField] private int N2 = 1;
+    [SerializeField] private float fadeSpeed = 3f;
 
     [Header("Assignables")]
     [Space(5)]
     [SerializeField] private TMPro.TextMeshPro n1Text = null;
     [SerializeField] private TMPro.TextMeshPro n2Text = null;
-    [SerializeField] private TMPro.TextMeshPro n3Text = null;
-
-    private AudioSource audioSource = null;
-    [SerializeField] private AudioClip clip = null;
+    [SerializeField] private Animator animator = null;
 
     private void Start()
     {
-        audioSource = GetComponent<AudioSource>();
-
         n1Text.text = N1.ToString();
         n2Text.text = N2.ToString();
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Q))
+        {
+            StopAllCoroutines();
+            StartCoroutine(FadeText(n1Text, false));
+        }        
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            StopAllCoroutines();
+            StartCoroutine(FadeText(n1Text, true));
+        }
+    }
+    public void StartAnimation()
+    {
+        if (!animator.GetCurrentAnimatorStateInfo(0).IsName("Bonk"))
+        {
+            animator.Play("Bonk");
+        }
     }
 
     public void GetNextFibonacciSequence()
@@ -32,9 +49,33 @@ public class Fibonacci : MonoBehaviour
 
         n1Text.text = N1.ToString();
         n2Text.text = N2.ToString();
+    }
 
-        n3Text.text = newNumber.ToString();
+/// <summary>
+/// Fade text in or out
+/// </summary>
+/// <param name="text"></param>
+/// <param name="fadeIn"></param>
+/// <returns></returns>
+    private IEnumerator FadeText(TMPro.TextMeshPro text, bool fadeIn)
+    {
+        int targetAlpha = fadeIn ? 1 : 0;
+        float startTime = Time.time;
+        float startAlpha = text.color.a;
 
-        audioSource.Play();
+        while (text.color.a != targetAlpha)
+        {
+            float timeSinceStarted = Time.time - startTime;
+            float progress = timeSinceStarted / fadeSpeed;
+
+            float newAlpha = Mathf.Lerp(startAlpha, targetAlpha, progress);
+
+            Color newColor = text.color;
+            newColor.a = newAlpha;
+
+            text.color = newColor;
+
+            yield return new WaitForFixedUpdate();
+        }
     }
 }
---
+++ The Fibonacci Project/Assets/Code/PlayBonkSound.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public class PlayBonkSound : MonoBehaviour
+{
+    private AudioSource source = null;
+    [SerializeField] private AudioClip bonk = null;
+
+    private void Start()
+    {
+        source = GetComponent<AudioSource>();
+    }
+    public void BonkSound()
+    {
+        source.PlayOneShot(bonk);
+    }
+}
